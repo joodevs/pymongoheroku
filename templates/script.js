037ef3dc83819148de4eac0632f81928e8c96ffb
Jooyoung Oh <jooyoungoh.03@gmail.com>
2022-02-09 19:14:09 +0300

    created script.js

---
+++ templates/script.js
@@ -0,0 +1,58 @@
+$(document).ready(function () {
+    showArticles();
+});
+
+function openClose() {
+    if ($("#post-box").css("display") == "block") {
+        $("#post-box").hide();
+        $("#btn-post-box").text("Open Posting Box");
+    } else {
+        $("#post-box").show();
+        $("#btn-post-box").text("Close Posting Box");
+    }
+}
+
+function postArticle() {
+    let url = $('#post-url').val()
+    let comment = $('#post-comment').val()
+
+    $.ajax({
+        type: "POST",
+        url: "/memo",
+        data: {url_give: url, comment_give: comment},
+        success: function (response) { // if successful
+            alert(response["msg"]);
+            window.location.reload()
+        }
+    })
+}
+
+function showArticles() {
+    $.ajax({
+        type: "GET",
+        url: "/memo",
+        data: {},
+        success: function (response) {
+            let articles = response['all_articles']
+            for (let i = 0; i < articles.length; i++) {
+                let title = articles[i]['title']
+                let image = articles[i]['image']
+                let url = articles[i]['url']
+                let desc = articles[i]['desc']
+                let comment = articles[i]['comment']
+
+                let temp_html = `<div class="card">
+                                                <img class="card-img-top"
+                                                     src="${image}"
+                                                     alt="Card image cap">
+                                                <div class="card-body">
+                                                    <a target="_blank" href="${url}" class="card-title">${title}</a>
+                                                    <p class="card-text">${desc}</p>
+                                                    <p class="card-text comment">${comment}</p>
+                                                </div>
+                                            </div>`
+                $('#cards-box').append(temp_html)
+            }
+        }
+    })
+}
